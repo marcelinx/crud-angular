b765e59cb8fc24f013b62e92fe5168248c4d3bbf
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2024-02-07 23:39:20 -0300

    feat: pipe para mostrar o icone

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -8,7 +8,7 @@ import { Course } from '../model/course';
   providedIn: 'root',
 })
 export class CoursesService {
-  private readonly API = '/assets/caourses.json';
+  private readonly API = '/assets/courses.json';
 
   constructor(private httpClient: HttpClient) {}
 
